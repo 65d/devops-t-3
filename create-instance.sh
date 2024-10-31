4ee51ab5df0183e5b449f1458700129240febf72
Myhailo Nyskohuz <61565249+65d@users.noreply.github.com>
2024-10-31 14:50:06 +0100

    Update create-instance.sh

--- create-instance.sh
+++ create-instance.sh
@@ -3,22 +3,42 @@
 IMAGE_ID="ami-01bc990364452ab3e"
 INSTANCE_TYPE="t2.micro"
 KEY_NAME=""
-SEQURITY_GROUP_ID=""
+SECURITY_GROUP_ID=""
 SUBNET_ID=""
-TAG_NAME="DZ3"
+TAG_NAME="$1"
 
+if [ -z "$TAG_NAME" ]; then
+    echo "Usage: $0 <tag-name>"
+    exit 1
+fi
+
+echo "Launching EC2 instance..."
 INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $IMAGE_ID \
     --instance-type $INSTANCE_TYPE \
     --key-name $KEY_NAME \
-    --security-group-ids $SEQURITY_GROUP_ID \
+    --security-group-ids $SECURITY_GROUP_ID \
     --subnet-id $SUBNET_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$TAG_NAME}]" \
+    --user-data file://user_data.sh \
     --query "Instances[0].InstanceId" \
     --output text)
 
-echo "Launching EC2 instance with ID: $INSTANCE_ID"
+echo "Instance ID: $INSTANCE_ID"
 
+echo "Waiting for instance to be ready..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+sleep 20
 
-echo "$INSTANCE_ID IS READY"
+echo "Instance is ready"
+
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+
+echo "Public IP: $PUBLIC_IP"
+
+echo "Connecting to EC2 instance..."
+ssh -i $KEY_NAME.pem -o StrictHostKeyChecking=no ec2-user@$PUBLIC_IP
+
+echo "Instance ID: $INSTANCE_ID"
+
+./terminate.sh "$INSTANCE_ID"
